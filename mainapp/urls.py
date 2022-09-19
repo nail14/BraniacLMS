620b894cbd70cea8751fa8888890ea9724a796ab
nail14 <nail_14@mail.ru>
2022-09-20 00:34:08 +0300

    lesson2 done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+
 from .views import *
 
-
 urlpatterns = [
-    path('hello', hello_view)
+    path("", MainPageView.as_view()),
+    path("login/", LoginPageView.as_view()),
+    path("doc_site/", DocSitePageView.as_view()),
+    path("contacts/", ContactPageView.as_view()),
+    path("news/", NewsPageView.as_view()),
+    path("courses_list/", CoursesPageView.as_view()),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.views.generic import TemplateView
 
-def hello_view(request):
-    return HttpResponse("Hello world")
+# def hello_view(request):
+#     return HttpResponse("Hello world")
+
+
+class MainPageView(TemplateView):
+    template_name = "index.html"
+
+
+class ContactPageView(TemplateView):
+    template_name = "contacts.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "courses_list.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "login.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "news.html"
