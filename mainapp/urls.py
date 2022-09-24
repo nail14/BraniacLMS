5ab7783ab239fe549db051362eb9e34dc41c4e81
nail14 <nail_14@mail.ru>
2022-09-24 23:13:29 +0300

    lesson-3 done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from .views import *
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", MainPageView.as_view()),
-    path("login/", LoginPageView.as_view()),
-    path("doc_site/", DocSitePageView.as_view()),
-    path("contacts/", ContactPageView.as_view()),
-    path("news/", NewsPageView.as_view()),
-    path("courses_list/", CoursesPageView.as_view()),
+    path("", MainPageView.as_view(), name='main'),
+    path("login/", LoginPageView.as_view(), name='login'),
+    path("doc_site/", DocSitePageView.as_view(), name='docs'),
+    path("contacts/", ContactPageView.as_view(), name='contacts'),
+    path("news/", NewsPageView.as_view(), name='news'),
+    # path("news/<int:pk>", NewsPageView.as_view(), name='news'),
+    path("courses_list/", CoursesPageView.as_view(), name='courses'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponse, render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from datetime import datetime
+
 
 # def hello_view(request):
 #     return HttpResponse("Hello world")
@@ -16,6 +18,9 @@ class ContactPageView(TemplateView):
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
+    # def get(self, request, word, *args, **kwargs):
+    #     super(CoursesPageView, self).get()
+
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
@@ -27,3 +32,12 @@ class LoginPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['news_title'] = 'Новость'
+        context['description'] = 'Предварительное описание новости'
+        context['news_date'] = datetime.now()
+        context['range'] = range(5)
+        return context
