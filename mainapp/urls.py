81374ef9eec5a4b707f2dc8188df2afdada7cd90
nail14 <nail_14@mail.ru>
2022-10-01 22:44:52 +0300

    lesson-4.4

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import *
+# from mainapp import views
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
@@ -9,8 +10,10 @@ urlpatterns = [
     path("", MainPageView.as_view(), name='main'),
     path("login/", LoginPageView.as_view(), name='login'),
     path("doc_site/", DocSitePageView.as_view(), name='docs'),
-    path("contacts/", ContactPageView.as_view(), name='contacts'),
+    path("contacts/", ContactsPageView.as_view(), name='contacts'),
     path("news/", NewsPageView.as_view(), name='news'),
-    path("news/<int:pk>", NewsPageView.as_view(), name='news_details'),
-    path("courses_list/", CoursesPageView.as_view(), name='courses'),
+    path("news/<int:pk>", NewsPageDetailView.as_view(), name='news_detail'),
+    path("courses/", CoursesListView.as_view(), name="courses"),
+    path("courses/<int:pk>/", CoursesDetailView.as_view(), name="courses_detail",),
+    path("courses_list/", CoursesListView.as_view(), name='courses'),
 ]
